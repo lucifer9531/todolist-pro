03a970b0cb7150c102f417fc1afab1758a4e1eb0
baixp <baixp@dustess.com>
2021-12-13 14:19:00 +0800

    feat: 升级react18

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-import './assets/css/resets.css'
-import './assets/css/border.css'
-import './assets/css/ui.css'
+import './assets/css/resets.css';
+import './assets/css/border.css';
+import './assets/css/ui.css';
 
-import './assets/js/common'
+import './assets/js/common';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
